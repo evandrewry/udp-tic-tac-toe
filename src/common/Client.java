b102231ea2e5d5c19556f76fd401c232be990330
Evan Drewry <evandrewry@gmail.com>
2013-02-11 23:24:27 -0500

    blajhakjfhl

--- src/common/Client.java
+++ src/common/Client.java
@@ -1,3 +1,4 @@
+package common;
 import packet.*;
 import packet.client.*;
 
--- src/common/Server.java
+++ src/common/Server.java
@@ -1,14 +1,16 @@
+package common;
 import java.util.ArrayList;
 import java.util.TreeSet;
 
 import common.Game;
 import common.User;
+import common.UserList;
 
 
 public class Server {
 
 
-	private TreeSet<User> currentUsers = new TreeSet<User>();
+	private UserList currentUsers = new UserList();
 	private ArrayList<Game> currentGames = new ArrayList<Game>();
 	
 	public static void main(String[] args) {
